3ffc7c6438692c26256ae45583f81887bec343e2
TTMac135 <tyler.mac@edu.sait.ca>
2024-09-24 17:11:16 -0600

    Created week-2 page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Tyler Mac</h1>
+        <Link href="https://github.com/TTMac135/cprg306-assignments">https://github.com</Link>
+      </main>
+    )
+}
