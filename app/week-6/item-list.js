ebd3aafc6695f2df871e259fb4e910a1c828e560
TTMac135 <tyler.mac@edu.sait.ca>
2024-10-22 22:09:59 -0600

    Week-6 Completed

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,95 +1,42 @@
+"use client";
+import { useState } from "react";
 import Item from "./item";
+import items from "./items.json";
 
 export default function ItemList() {
-  const item1 = {
-    name: "milk, 4 L 🥛",
-    quantity: 1,
-    category: "dairy",
-  };
-   
-  const item2 = {
-    name: "bread 🍞",
-    quantity: 2,
-    category: "bakery",
-  };
-   
-  const item3 = {
-    name: "eggs, dozen 🥚",
-    quantity: 2,
-    category: "dairy",
-  };
-   
-  const item4 = {
-    name: "bananas 🍌",
-    quantity: 6,
-    category: "produce",
-  };
-   
-  const item5 = {
-    name: "broccoli 🥦",
-    quantity: 3,
-    category: "produce",
-  };
-   
-  const item6 = {
-    name: "chicken breasts, 1 kg 🍗",
-    quantity: 1,
-    category: "meat",
-  };
-   
-  const item7 = {
-    name: "pasta sauce 🍝",
-    quantity: 3,
-    category: "canned goods",
-  };
-   
-  const item8 = {
-    name: "spaghetti, 454 g 🍝",
-    quantity: 2,
-    category: "dry goods",
-  };
-   
-  const item9 = {
-    name: "toilet paper, 12 pack 🧻",
-    quantity: 1,
-    category: "household",
-  };
-   
-  const item10 = {
-    name: "paper towels, 6 pack",
-    quantity: 1,
-    category: "household",
-  };
-   
-  const item11 = {
-    name: "dish soap 🍽️",
-    quantity: 1,
-    category: "household",
-  };
-   
-  const item12 = {
-    name: "hand soap 🧼",
-    quantity: 4,
-    category: "household",
-  };
+  
+  const [sortBy, setSortBy] = useState("name");
+  const itemArray = [...items];
 
-  return (
-    <main className="p-3 ml-2">
-      <ul>
-        <li><Item {...item1}></Item></li>
-        <li><Item {...item2}></Item></li>
-        <li><Item {...item3}></Item></li>
-        <li><Item {...item4}></Item></li>
-        <li><Item {...item5}></Item></li>
-        <li><Item {...item6}></Item></li>
-        <li><Item {...item7}></Item></li>
-        <li><Item {...item8}></Item></li>
-        <li><Item {...item9}></Item></li>
-        <li><Item {...item10}></Item></li>
-        <li><Item {...item11}></Item></li>
-        <li><Item {...item12}></Item></li>
-      </ul>
+  itemArray.sort((a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } 
+
+    else if (sortBy === "category") 
+    {
+      return a.category.localeCompare(b.category);
+    }
+    return 0;
+  });
+    
+  const handleNameSort = () => setSortBy("name");
+  const handleCategorySort = () => setSortBy("category");
+
+  const NameSorted = sortBy === "name";
+  const CategorySorted = sortBy === "category";
+
+  return(        
+    <main>
+      <div>
+        <label>Sort by:</label>
+        <button onClick={handleNameSort} className={`bg-orange-500 hover:bg-orange-700 p-1 m-2 w-28 ${NameSorted}`}>Name</button>
+        <button onClick={handleCategorySort} className={`bg-orange-500 hover:bg-orange-700 p-1 m-2 w-28 ${CategorySorted}`}>Category</button>
+      </div>
+
+      <section>
+        {itemArray.map((item) => (<Item item={item} key={item.id} />))}
+      </section>
     </main>
   )
-
 }
--- app/week-6/item.js
+++ app/week-6/item.js
@@ -1,9 +1,9 @@
-export default function Item({name, quantity, category}) {
+export default function Item({item}) {
   return (
     <div>
       <ul className="bg-slate-900 p-2 max-w-sm mb-3">
-        <li className="text-xl font-bold">{name}</li>
-        <li>Buy {quantity} in {category}</li>
+        <li className="text-xl font-bold">{item.name}</li>
+        <li>Buy {item.quantity} in {item.category}</li>
       </ul>
     </div>
   )
